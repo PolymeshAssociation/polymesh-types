6a119cd2763d093af5ff64507448127bc62681e3
Prashant Bajpai <prashant.bajpai19@gmail.com>
2023-03-14 23:39:58 +0530

    feat: 🎸 Export typesBundle for various runtime env

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
+import { OverrideBundleDefinition } from '@polkadot/types/types';
+
 import rpc from './rpc';
 import signedExtensions from './signedExtensions';
 import types from './types';
 
-export default {
+const specTypes: OverrideBundleDefinition = {
   rpc,
   types,
   signedExtensions,
+};
+
+export const typesBundle = {
+  spec: {
+    polymesh_dev: specTypes,
+    polymesh: specTypes,
+    polymesh_ci: specTypes,
+    polymesh_mainnet: specTypes,
+    polymesh_testnet: specTypes,
+  },
 };
